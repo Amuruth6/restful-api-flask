d7f8a200365cd0bc851bf57c8581b864b00790f5
Amuruth R <144511338+Amuruth6@users.noreply.github.com>
2026-01-07 19:29:02 +0530

    Add files via upload

---
+++ server.py
@@ -0,0 +1,179 @@
+from flask import Flask, url_for
+from flask import request
+from flask import json
+from flask import Response
+from flask import jsonify
+from functools import wraps
+import logging
+
+app = Flask(__name__)
+
+
+# Basic Routes
+
+
+@app.route('/')
+def api_root():
+    return 'Welcome'
+
+
+@app.route('/articles')
+def api_articles():
+    return 'List of ' + url_for('api_articles')
+
+
+@app.route('/articles/<articleid>')
+def api_article(articleid):
+    return 'You are reading ' + articleid
+
+
+
+# GET Parameters
+
+
+@app.route('/hello')
+def api_hello_param():
+    if 'name' in request.args:
+        return 'Hello ' + request.args['name']
+    else:
+        return 'Hello Amuruth'
+
+
+
+# HTTP Methods
+
+
+@app.route('/echo', methods=['GET', 'POST', 'PATCH', 'PUT', 'DELETE'])
+def api_echo():
+    if request.method == 'GET':
+        return "ECHO: GET\n"
+
+    elif request.method == 'POST':
+        return "ECHO: POST\n"
+
+    elif request.method == 'PATCH':
+        return "ECHO: PACTH\n"
+
+    elif request.method == 'PUT':
+        return "ECHO: PUT\n"
+
+    elif request.method == 'DELETE':
+        return "ECHO: DELETE\n"
+
+
+
+# request data
+
+
+@app.route('/messages', methods=['POST'])
+def api_message():
+
+    if request.headers['Content-Type'] == 'text/plain':
+        return "Text Message: " + request.data.decode()
+
+    elif request.headers['Content-Type'] == 'application/json':
+        return "JSON Message: " + json.dumps(request.json)
+
+    elif request.headers['Content-Type'] == 'application/octet-stream':
+        f = open('./binary', 'wb')
+        f.write(request.data)
+        f.close()
+        return "Binary message written!"
+
+    else:
+        return "415 Unsupported Media Type :)"
+
+
+
+# JSON response
+
+
+@app.route('/hello_json', methods=['GET'])
+def api_hello_json():
+    data = {
+        'hello': 'world',
+        'number': 3
+    }
+    js = json.dumps(data)
+
+    resp = Response(js, status=200, mimetype='application/json')
+    resp.headers['Link'] = 'http://luisrei.com'
+    return resp
+
+
+
+# error handling
+
+
+@app.errorhandler(404)
+def not_found(error=None):
+    message = {
+        'status': 404,
+        'message': 'Not Found: ' + request.url
+    }
+    resp = jsonify(message)
+    resp.status_code = 404
+    return resp
+
+
+@app.route('/users/<userid>', methods=['GET'])
+def api_users(userid):
+    users = {'1': 'ari', '2': 'krathiban', '3': 'praga'}
+
+    if userid in users:
+        return jsonify({userid: users[userid]})
+    else:
+        return not_found()
+
+
+# authentication
+
+def check_auth(username, password):
+    return username == 'admin' and password == 'secret'
+
+
+def authenticate():
+    message = {'message': "Authenticate."}
+    resp = jsonify(message)
+    resp.status_code = 404
+    resp.headers['WWW-Authenticate'] = 'Basic realm="Example"'
+    return resp
+
+
+def requires_auth(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        auth = request.authorization
+        if not auth:
+            return authenticate()
+        elif not check_auth(auth.username, auth.password):
+            return authenticate()
+        return f(*args, **kwargs)
+    return decorated
+
+
+@app.route('/secrets')
+@requires_auth
+def api_secret():
+    return "this is top secret stuff!"
+
+
+
+#loginning
+
+file_handler = logging.FileHandler('app.log')
+app.logger.addHandler(file_handler)
+app.logger.setLevel(logging.INFO)
+
+
+@app.route('/logs', methods=['GET'])
+def api_logs():
+    app.logger.info('informing')
+    app.logger.warning('warning')
+    app.logger.error('screaming bloody murder!')
+    return "check your logs\n"
+
+#run server
+
+if __name__ == '__main__':
+    app.run(debug=True)
